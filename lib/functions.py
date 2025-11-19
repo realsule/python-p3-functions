08743b93ca5ebab17be7392c9d12829c5828deaf
Suleiman <real1sule@gmail.com>
2025-11-19 11:15:03 +0300

    complete python-p3-functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
-def greet_programmer():
-    pass
+def greet_programmer(name="programmer"):
+    print(f"Hello, {name}!")
+
+greet_programmer()
+    
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("sule")    
+    
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default("sule")    
+    
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
 
 def halve(number):
-    pass
+    return number / 2
